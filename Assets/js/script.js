20b3035ff5f5c557b36ea933e8ea35f23bdedf48
malloryklar0516 <malloryklar@Mallorys-Air.cable.rcn.com>
2023-08-31 22:49:32 -0400

    added javascript, css and html

---
+++ Assets/js/script.js
@@ -0,0 +1,85 @@
+$(document).ready(function(){ 
+    var apiKey = '&appid=2be56aada7872d75001afb030b365d8e';
+    var searchBtn = document.getElementById('searchbutton');
+    var historyEl= document.querySelector('.search-history');
+
+   if (localStorage){
+    var historyList = JSON.parse(localStorage.getItem('historyList'))||[];
+    //displays 8 recent searches
+    var displayList = historyList.slice(0,9);
+    for (i=0; i< displayList.length; i++){
+        $('.search-history').append("<li>"+displayList[i]+"</li>" );
+    }
+    displayList=[];
+   //click event listener for the search button feature, calls getWeather function  
+    searchBtn.addEventListener('click', function(){
+        var searchEl=document.querySelector('#searchcity').value;
+        localStorage.setItem('storedCity',searchEl);
+        var city = localStorage.getItem('storedCity');
+        historyList.unshift(city);
+        localStorage.setItem('historyList',JSON.stringify(historyList));
+        getWeather(city);         
+        })
+// gets weather and forecast when cities in recent searches are clicked
+        $('.search-history').on("click","li", function(){
+            getWeather($(this).text());
+        })
+       };
+// gets today's date
+   function todaysDate(){
+    var today = dayjs().format("MM/DD/YYYY");
+    return today;
+   }
+// function that gets weather and forecast for city searched and adds to html
+    function getWeather(city){
+    var weatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial"+apiKey;
+    $.ajax({
+    url: weatherURL,
+    method: "GET"
+    })
+   .then(function(response){
+       $('.city').html("<h3>"+ response.name+ " ("+ todaysDate()+")" +"</h3>");
+       $('.icon').html("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' >");
+       $('.temp').text("Temperature: "+response.main.temp+" F");
+       $('.wind').text("Wind: " +response.wind.speed +" MPH");
+       $('.hum').text("Humidity: "+response.main.humidity + "%");
+        var latCoord = response.coord.lat;
+        var longCoord = response.coord.lon;
+        var forecastURL="https://api.openweathermap.org/data/2.5/forecast?lat="+latCoord+"&lon="+longCoord +"&units=imperial"+apiKey;
+        //ajax call to get forecast for city searched 
+        $.ajax({
+            url: forecastURL,
+            method: "GET"
+        }).then(function(response){
+            $('.day1-date').html("<h4>"+ dayjs(response.list[0].dt_txt).format( "MM/DD/YYYY")+"</h4>");
+            $('.day1-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[0].weather[0].icon +".png'>");
+            $('.day1-temp').text("Temp: "+response.list[0].main.temp+" F");
+            $('.day1-wind').text("Wind: "+ response.list[0].wind.speed +" MPH");
+            $('.day1-humidity').text("Humidity: "+response.list[0].main.humidity +"%");
+            $('.day2-date').html("<h4>"+ dayjs(response.list[8].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day2-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[8].weather[0].icon +".png'>");
+            $('.day2-temp').text("Temp: "+response.list[8].main.temp+" F");
+            $('.day2-wind').text("Wind: "+ response.list[8].wind.speed +" MPH");
+            $('.day2-humidity').text("Humidity: "+response.list[8].main.humidity +"%");
+            $('.day3-date').html("<h4>"+ dayjs(response.list[16].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day3-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[16].weather[0].icon +".png'>");
+            $('.day3-temp').text("Temp: "+response.list[16].main.temp+" F");
+            $('.day3-wind').text("Wind: "+ response.list[16].wind.speed +" MPH");
+            $('.day3-humidity').text("Humidity: "+response.list[16].main.humidity +"%");
+            $('.day4-date').html("<h4>"+ dayjs(response.list[24].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day4-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[24].weather[0].icon +".png'>");
+            $('.day4-temp').text("Temp: "+response.list[24].main.temp+" F");
+            $('.day4-wind').text("Wind: "+ response.list[24].wind.speed +" MPH");
+            $('.day4-humidity').text("Humidity: "+response.list[24].main.humidity +"%");
+            $('.day5-date').html("<h4>"+ dayjs(response.list[31].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day5-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[31].weather[0].icon +".png'>");
+            $('.day5-temp').text("Temp: "+response.list[31].main.temp+" F");
+            $('.day5-wind').text("Wind: "+ response.list[31].wind.speed +" MPH");
+            $('.day5-humidity').text("Humidity: "+response.list[31].main.humidity +"%");
+          console.log(response.list);  
+        });
+   });
+   };
+    })
+
+
--- README.md
+++ README.md
@@ -1,1 +1,43 @@
-# weather-dashboard
+# Challenge 5 Weather Dashboard
+## Description
+Weather Dashboard that runs in browser and updates dynamically in  HTML and CSS by search
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+## Deployment
+### Deployment Link
+### Deployed Page
+
+## Technologies Used
+CSS, JavaScript, HTML, Bootstrap, Jquery, Third-Party APIs, DayJS
+
+## License
+© 2023 Mallory Klar malloryklar0516
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
