c09bcb481ec36b4390f6cdb1e445e1548691ad3b
malloryklar0516 <malloryklar@Mallorys-MacBook-Air.local>
2024-07-17 14:15:38 -0400

    style updates

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -39,7 +39,7 @@ $(document).ready(function(){
     method: "GET"
     })
    .then(function(response){
-       $('.city').html("<h3>"+ response.name+ " ("+ todaysDate()+")" +"</h3>");
+       $('.city').html("<h3 class='fw-bold'>"+ response.name+ " ("+ todaysDate()+")" +"</h3>");
        $('.icon').html("<img src='https://openweathermap.org/img/w/" + response.weather[0].icon + ".png' >");
        $('.temp').text("Temperature: "+response.main.temp+" F");
        $('.wind').text("Wind: " +response.wind.speed +" MPH");
@@ -52,27 +52,27 @@ $(document).ready(function(){
             url: forecastURL,
             method: "GET"
         }).then(function(response){
-            $('.day1-date').html("<h4>"+ dayjs(response.list[0].dt_txt).format( "MM/DD/YYYY")+"</h4>");
+            $('.day1-date').html("<h5 class='h5 fw-bold'>"+ dayjs(response.list[0].dt_txt).format( "MM/DD/YYYY")+"</h5>");
             $('.day1-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[0].weather[0].icon +".png'>");
             $('.day1-temp').text("Temp: "+response.list[0].main.temp+" F");
             $('.day1-wind').text("Wind: "+ response.list[0].wind.speed +" MPH");
             $('.day1-humidity').text("Humidity: "+response.list[0].main.humidity +"%");
-            $('.day2-date').html("<h4>"+ dayjs(response.list[8].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day2-date').html("<h5 class='h5 fw-bold'>"+ dayjs(response.list[8].dt_txt).format("MM/DD/YYYY")+"</h5>");
             $('.day2-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[8].weather[0].icon +".png'>");
             $('.day2-temp').text("Temp: "+response.list[8].main.temp+" F");
             $('.day2-wind').text("Wind: "+ response.list[8].wind.speed +" MPH");
             $('.day2-humidity').text("Humidity: "+response.list[8].main.humidity +"%");
-            $('.day3-date').html("<h4>"+ dayjs(response.list[16].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day3-date').html("<h5 class='h5 fw-bold'>"+ dayjs(response.list[16].dt_txt).format("MM/DD/YYYY")+"</h5>");
             $('.day3-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[16].weather[0].icon +".png'>");
             $('.day3-temp').text("Temp: "+response.list[16].main.temp+" F");
             $('.day3-wind').text("Wind: "+ response.list[16].wind.speed +" MPH");
             $('.day3-humidity').text("Humidity: "+response.list[16].main.humidity +"%");
-            $('.day4-date').html("<h4>"+ dayjs(response.list[24].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day4-date').html("<h5 class='h5 fw-bold'>"+ dayjs(response.list[24].dt_txt).format("MM/DD/YYYY")+"</h5>");
             $('.day4-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[24].weather[0].icon +".png'>");
             $('.day4-temp').text("Temp: "+response.list[24].main.temp+" F");
             $('.day4-wind').text("Wind: "+ response.list[24].wind.speed +" MPH");
             $('.day4-humidity').text("Humidity: "+response.list[24].main.humidity +"%");
-            $('.day5-date').html("<h4>"+ dayjs(response.list[31].dt_txt).format("MM/DD/YYYY")+"</h4>");
+            $('.day5-date').html("<h5 class='h5 fw-bold'>"+ dayjs(response.list[31].dt_txt).format("MM/DD/YYYY")+"</h5>");
             $('.day5-icon').html("<img src='https://openweathermap.org/img/w/"+response.list[31].weather[0].icon +".png'>");
             $('.day5-temp').text("Temp: "+response.list[31].main.temp+" F");
             $('.day5-wind').text("Wind: "+ response.list[31].wind.speed +" MPH");
